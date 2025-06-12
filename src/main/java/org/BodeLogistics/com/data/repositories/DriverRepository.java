861892bcfdc53c98cef1ec00bf2321160ef0a3d6
sulaimonlawal <lawalSulaimon003@gmail.com>
2025-06-12 10:01:17 +0100

    driver repository and registerDriver service working with unit test

---
+++ src/main/java/org/BodeLogistics/com/data/repositories/DriverRepository.java
@@ -0,0 +1,16 @@
+package org.BodeLogistics.com.data.repositories;
+
+import org.BodeLogistics.com.data.models.Driver;
+import org.BodeLogistics.com.data.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DriverRepository extends MongoRepository<Driver, String> {
+    boolean existsByEmail(String email);
+    Optional<Driver> findByEmail(String email);
+    Optional<Driver> findByPhoneNumber(String phoneNumber);
+    boolean existsByPhoneNumber(String phoneNumber);
+}
