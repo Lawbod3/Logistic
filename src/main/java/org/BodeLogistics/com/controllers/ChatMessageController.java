1e561dde7a810d34387d05674e535e7879659fd2
sulaimonlawal <lawalSulaimon003@gmail.com>
2025-06-17 20:46:46 +0100

    chat activate api working

--- src/main/java/org/BodeLogistics/com/controllers/ChatMessageController.java
+++ src/main/java/org/BodeLogistics/com/controllers/ChatMessageController.java
@@ -1,11 +1,17 @@
 package org.BodeLogistics.com.controllers;
 
+import jakarta.validation.Valid;
+import org.BodeLogistics.com.dto.request.SetChatToActiveRequest;
+import org.BodeLogistics.com.dto.response.ApiResponse;
+import org.BodeLogistics.com.dto.response.SetChatToActiveResponse;
+import org.BodeLogistics.com.exceptions.RideActivityDoesNotExist;
 import org.BodeLogistics.com.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -18,5 +24,30 @@ public class ChatMessageController {
     public String test() {
         return "Api is working";
     }
+
+    @PostMapping("/login/ride-activity/activate-chat")
+    public ResponseEntity<?> activateChat(@Valid @RequestBody SetChatToActiveRequest request) {
+        try{
+            SetChatToActiveResponse response = chatService.setChatToActive(request);
+            return new ResponseEntity<>(new ApiResponse(true, response), HttpStatus.CREATED);
+
+        }
+        catch(RideActivityDoesNotExist e){
+            return new ResponseEntity<>(new ApiResponse(false, e), HttpStatus.BAD_REQUEST);
+
+        }
+    }
+
+    private ResponseEntity<?> createErrorResponse(Exception e, HttpStatus status) {
+        return new ResponseEntity<>(new ApiResponse(false, e.getMessage()), status);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException e) {
+        FieldError errorMessage = e.getBindingResult().getFieldError();
+        return createErrorResponse(new Exception(errorMessage.getDefaultMessage()), HttpStatus.BAD_REQUEST);
+    }
+
 
 }
--- src/main/java/org/BodeLogistics/com/dto/request/SetChatToActiveRequest.java
+++ src/main/java/org/BodeLogistics/com/dto/request/SetChatToActiveRequest.java
@@ -1,9 +1,11 @@
 package org.BodeLogistics.com.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class SetChatToActiveRequest {
+    @NotBlank
     private String activityId;
     private String driverId;
     private String userId;
